57dee2749ab740165fc5278b9ca594e51bb81908
Dzhanybek Zakiriiaev <dzhanybek.zakiriiaev@gmail.com>
2023-05-03 17:05:42 +0600

    created services

---
+++ src/main/java/com/example/homework65/service/ProductService.java
@@ -0,0 +1,40 @@
+package com.example.homework65.service;
+
+import com.example.homework65.entity.Product;
+import com.example.homework65.repository.product.ProductRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public Product findProductByName(String name) {
+        return productRepository.findByName(name);
+    }
+
+    @Transactional(readOnly = true)
+    public Product findProductByNumber(Integer number) {
+        return productRepository.findByNumber(number);
+    }
+
+    public void saveProduct(Product product) {
+        productRepository.save(product);
+    }
+
+    public void deleteProductById(Integer id) {
+        productRepository.deleteById(id);
+    }
+
+    public List<Product> findAllProducts() {
+        return productRepository.findAll();
+    }
+
+}
---
+++ src/main/java/com/example/homework65/service/UserService.java
@@ -0,0 +1,50 @@
+package com.example.homework65.service;
+
+import com.example.homework65.entity.User;
+import com.example.homework65.repository.user.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public User userByUsername(String username){
+        User byUserName = userRepository.findByUsername(username);
+        return byUserName;
+    }
+
+    @Transactional(readOnly = true)
+    public User userByEmail(String email){
+        User byEmail = userRepository.findByEmail(email);
+        return byEmail;
+    }
+
+    @Transactional(readOnly = true)
+    public User findById(Integer id) {
+        Optional<User> userOptional = userRepository.findById(Long.valueOf(id));
+        return userOptional.orElse(null);
+    }
+
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    public void deleteUserById(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+}
