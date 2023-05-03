346c4b322cbf25fb5989b17600a98c90f5bfcc04
Dzhanybek Zakiriiaev <dzhanybek.zakiriiaev@gmail.com>
2023-05-03 16:45:24 +0600

    added methods

--- src/main/java/com/example/homework65/repository/product/ProductRepository.java
+++ src/main/java/com/example/homework65/repository/product/ProductRepository.java
@@ -4,4 +4,6 @@ import com.example.homework65.entity.Product;
 import com.example.homework65.repository.common.BaseRepository;
 
 public interface ProductRepository extends BaseRepository<Product, Integer> {
+    Product findByName(String name);
+    Product findByNumber(Integer number);
 }
--- src/main/java/com/example/homework65/repository/user/UserRepository.java
+++ src/main/java/com/example/homework65/repository/user/UserRepository.java
@@ -2,7 +2,20 @@ package com.example.homework65.repository.user;
 
 import com.example.homework65.entity.User;
 import com.example.homework65.repository.common.BaseRepository;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.management.relation.Role;
+import java.util.List;
 
 public interface UserRepository extends BaseRepository<User, Long> {
+    User findByUsername(String username);
+    User findByEmail(String email);
+
+    @Query("SELECT DISTINCT r FROM User u JOIN u.roles r WHERE u.id = :userId")
+    List<Role> findRolesByUserId(Long userId);
+
+    @EntityGraph(attributePaths = "roles")
+    User getById(Long id);
 }
 
